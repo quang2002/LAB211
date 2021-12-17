a12b66d3f314f211eb1c6680b99701f7449c8e05
quang2002 <clone27112002@gmail.com>
2021-12-17 21:21:55 +0700

    done: J1.S.P0001

---
+++ J1.L.P0021/src/Student/StudentValidation.java
@@ -0,0 +1,24 @@
+package Student;
+
+public class StudentValidation {
+
+    public static boolean isValidID(String id) {
+        return true;
+    }
+
+    public static boolean isValidStudentName(String studentName) {
+        return true;
+    }
+
+    public static boolean isValidSemester(int semester) {
+        return true;
+    }
+
+    public static boolean isValidCourseName(String courseName) {
+        return true;
+    }
+
+    public static boolean isValidStudent(Student student) {
+        return true;
+    }
+}
